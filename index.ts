7d6d7625aefc6f8d4595d4530a14c238753f1979
ChiHo <xchiho1ii@gmail.com>
2025-01-09 19:16:24 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,26 +4,17 @@ import * as fs from 'fs';
 
 const url = "https://bgm.tv";
 
-
-interface ImageUrls {
-    small: string;
-    grid: string;
-    large: string;
-    medium: string;
-    common: string;
-}
-
 interface Anime {
     id: string;
     title: string;
-    imageUrl: ImageUrls;
+    imageUrl: string;
     followers: number;
-}
+};
 
 interface AnimeCategory {
     type: string;
     items: Anime[];
-}
+};
 
 async function fetchAnimeData(url: string): Promise<AnimeCategory[]> {
     try {
@@ -35,30 +26,20 @@ async function fetchAnimeData(url: string): Promise<AnimeCategory[]> {
             const type = $(element).find('h2.title').text().trim();
             const items: Anime[] = [];
 
-            $(element).find('.mainItem').each(async(_, item) => {
+            $(element).find('.mainItem').each((_, item) => {
                 const title = $(item).find('a').attr('title')?.trim() || '';
                 const href = $(item).find('a').attr('href') || '';
                 const id = href.match(/subject\/(\d+)/)?.[1] || '';
-                const apiUrl = `https://api.bgm.tv/v0/subjects/${id}`;
-                const apiResponse = await axios.get(apiUrl,{
-                    headers: {
-                        'User-Agent': 'CcchiiiiHo/bgm_home_subject_data (https://github.com/CcchiiiiHo/bgm_home_subject_data)'
-                    }});
-                const imageUrl: ImageUrls = apiResponse.data.images;
+                const imageUrl = $(item).find('.image').attr('style')?.trim().replace('background-image:url(//lain.bgm.tv', 'https://lain.bgm.tv').replace(");","") || '';
                 const followers = parseInt($(item).find('.info small.grey').text().replace(/[^0-9]/g, ''), 10);
                 items.push({ id, title, imageUrl, followers });
             });
 
-            $(element).find('.subitem.clearit').each(async(_, item) => {
+            $(element).find('.subitem.clearit').each((_, item) => {
                 const title = $(item).find('.title a').text().trim();
                 const href = $(item).find('.title a').attr('href') || '';
                 const id = href.match(/subject\/(\d+)/)?.[1] || '';
-                const apiUrl = `https://api.bgm.tv/v0/subjects/${id}`;
-                const apiResponse = await axios.get(apiUrl,{
-                    headers: {
-                        'User-Agent': 'CcchiiiiHo/bgm_home_subject_data (https://github.com/CcchiiiiHo/bgm_home_subject_data)'
-                    }});
-                const imageUrl: ImageUrls = apiResponse.data.images;
+                const imageUrl = $(item).find('a').attr('style')?.trim().replace("background-image:url('//lain.bgm.tv", 'https://lain.bgm.tv').replace("');","") || '';
                 const followers = parseInt($(item).find('.inner small.grey').text().replace(/[^0-9]/g, ''), 10);
                 items.push({ id, title, imageUrl, followers });
             });
@@ -70,8 +51,8 @@ async function fetchAnimeData(url: string): Promise<AnimeCategory[]> {
     } catch (error) {
         console.error('Error fetching data:', error);
         return [];
-    }
-}
+    };
+};
 
 fetchAnimeData(url).then(animeCategories => {
     fs.writeFileSync('animeData.json', JSON.stringify(animeCategories, null, 2));
